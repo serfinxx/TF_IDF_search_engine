7a9f52cf993c6cbf45c8859bafc00c1ccf56bf64
serfinxx <serfinxx@gmail.com>
2021-06-19 06:48:07 +0100

    clustering updated

--- src/tf_idf_clustering.py
+++ src/tf_idf_clustering.py
@@ -1,44 +1,39 @@
-from sklearn.cluster import KMeans
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.cluster import MiniBatchKMeans
 from sklearn.decomposition import PCA
-from sklearn.manifold import TSNE
 
+import pandas as pd
 import matplotlib.pyplot as plt
 
-# {'anger': 0, 'love': 1, 'fear': 2, 'joy': 3, 'sadness': 4,'surprise': 5}
-class TfIdfClustering:
-    def plot(self):
-        # create k-means model to plot
-        num_clusters = 6
-        max_iterations = 300
-        labels_color_map = {
-            0: '#20b2aa', 1: '#ff7373', 2: '#ffe4e1', 3: '#005073', 4: '#4d0404',
-            5: '#ccc0ba', 6: '#4700f9', 7: '#f6f900', 8: '#00f91d', 9: '#da8c49'
-        }
-        pca_num_components = 2
-        tsne_num_components = 2
+doc = pd.read_csv("../data/train.txt", sep=';').iloc[:, 0]
+labels = pd.read_csv("../data/train.txt", sep=';').iloc[:, 1]
+dictionary = {'anger': 0, 'love': 1, 'fear': 2, 'joy': 3, 'sadness': 4, 'surprise': 5}
+label_index = [dictionary[x] for x in labels]
 
-        clustering_model = KMeans(
-            n_clusters=num_clusters,
-            max_iter=max_iterations,
-            precompute_distances="auto",
-            n_jobs=-1
-        )
+vectorizer = TfidfVectorizer(stop_words='english')
+tf_idf = vectorizer.fit_transform(doc)
 
-        labels = clustering_model.fit_predict(self.tf_idf)
-        dense_tfidf = self.tf_idf.todense()
+true_k = 6
+random_state = 0
 
-        reduced_data = PCA(n_components=pca_num_components).fit_transform(dense_tfidf)
+model = MiniBatchKMeans(n_clusters=true_k, random_state=random_state, max_iter=300)
+model.fit(tf_idf)
+# print(model.labels_)
 
-        fig, ax = plt.subplots()
-        for index, instance in enumerate(reduced_data):
-            # print instance, index, labels[index]
-            pca_comp_1, pca_comp_2 = reduced_data[index]
-            color = labels_color_map[labels[index]]
-            ax.scatter(pca_comp_1, pca_comp_2, c=color)
-        plt.show()
+# dimensionality reduction to 2D for visualisation
+pca = PCA(n_components=2, random_state=random_state)
+reduced_tf_idf = pca.fit_transform(tf_idf.toarray())
 
-        # t-SNE plot
-        embeddings = TSNE(n_components=tsne_num_components)
-        Y = embeddings.fit_transform(dense_tfidf)
-        plt.scatter(Y[:, 0], Y[:, 1], cmap=plt.cm.Spectral)
-        plt.show()
+# reduce the cluster centres to 2D
+reduced_cluster_centres = pca.transform(model.cluster_centers_)
+
+# print(model.predict(tf_idf))
+# plot
+plt.scatter(reduced_tf_idf[:, 0], reduced_tf_idf[:, 1], c=model.predict(tf_idf))
+# cluster centres
+plt.scatter(reduced_cluster_centres[:, 0], reduced_cluster_centres[:, 1], marker='x', s=150, c='b')
+plt.savefig("../results/%s" % "clustering", format="png", dpi=1080)
+plt.show()
+
+
+
